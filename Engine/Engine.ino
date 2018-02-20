acb33272c90d82173a9ca70ff9b12eec3399a6c0
Eivind <eivinds.erichsen@gmail.com>
2018-02-20 19:05:01 +0100

    tryms update

--- Engine/Engine.ino
+++ Engine/Engine.ino
@@ -2,90 +2,106 @@
 #include "RF24.h"
 #include "SPI.h"
 
-/* Arduino / Radio code*/
+// CONSTANTS
+
+// Pins
+#define RIGHT_ENABLE 3
+#define RIGHT_FORWARD 4
+#define RIGHT_BACKWARD 6
+#define LEFT_ENABLE 5
+#define LEFT_FORWARD 8
+#define LEFT_BACKWARD 7
+#define LAZER 2
+
+// Safety
+#define TIMEOUT 1000 // Maximum waiting time in milliseconds
+
+// VARIABLES
 
 RF24 radio(9, 10);
 const uint64_t pipe = 0xE6E6E6E6E6E6;
 
-// V A R I A B L E S
 int movement[5];
 int DirX = 0;
 int DirY = 0;
 int speeds = 0;
 int turn = 0;
 
-// P I N S
-int RIGHT_ENABLE = 3;
-int RIGHT_FORWARD = 4;
-int RIGHT_BACKWARD = 6;
-int LEFT_ENABLE = 5;
-int LEFT_FORWARD = 8;
-int LEFT_BACKWARD = 7;
-int LAZER = 2;
+int motor_speed = 0;
+int motor_turn = 0;
+unsigned long timestamp = 0;
 
 void setup(void) {
-    Serial.begin(9600);
-    Serial.println("Startup");
-    
-   if (!radio.begin()) {
-      Serial.println("Begin false!!");
+    if (!radio.begin()) {
+      //Serial.println("Begin false!!");
     } else {
-      Serial.println("Stash");
+      //Serial.println("Stash");
     }
     radio.setChannel(95);
     radio.openReadingPipe(1, pipe); // Get NRF24L01 ready to receive
     radio.startListening(); // Listen to see if information received
 
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
+    pinMode(RIGHT_ENABLE, OUTPUT);
+    pinMode(RIGHT_FORWARD, OUTPUT);
+    pinMode(RIGHT_BACKWARD, OUTPUT);
+    pinMode(LEFT_ENABLE, OUTPUT);
+    pinMode(LEFT_FORWARD, OUTPUT);
+    pinMode(LEFT_BACKWARD, OUTPUT);
+    pinMode(LAZER, OUTPUT);
+
+    //Serial.begin(9600);
+    //Serial.println("Startup");
     
+    timestamp = millis();
 }
 
 void loop(void) {
-  
     while (radio.available()) {
-    radio.read(&movement, sizeof(movement));
-    
-        for(int i = 0;i<4;i++){
-           //Serial.print(movement[i]);
-        }
-    }
-    
-    speeds = movement[2];
-    turn = movement[1];
-    
-    if( speeds > 0 ){
+      radio.read(&movement, sizeof(movement));
+      
+      speeds = movement[2];
+      turn = movement[1];
 
-      digitalWrite(RIGHT_FORWARD, HIGH);
-      digitalWrite(RIGHT_BACKWARD, LOW);
-      analogWrite(RIGHT_ENABLE, 25*abs(speeds-turn));
-      digitalWrite(LEFT_FORWARD, HIGH);
-      digitalWrite(LEFT_BACKWARD, LOW);
-      analogWrite(LEFT_ENABLE, 25*abs(speeds+turn));
-    }
-    else if( speeds < 0 ){ 
-      speeds=abs(speeds);
-      digitalWrite(RIGHT_FORWARD, LOW);
-      digitalWrite(RIGHT_BACKWARD, HIGH);
-      analogWrite(RIGHT_ENABLE, 25*abs(speeds-turn));
-      digitalWrite(LEFT_FORWARD, LOW);
-      digitalWrite(LEFT_BACKWARD, HIGH);
-      analogWrite(LEFT_ENABLE, 25*abs(speeds+turn));
-    }
-    else {
-      digitalWrite(RIGHT_FORWARD, HIGH);
-      digitalWrite(RIGHT_BACKWARD, HIGH);
-      digitalWrite(LEFT_FORWARD, LOW);
-      digitalWrite(LEFT_BACKWARD, LOW);
+      motorSpeed(speeds);
+      motorTurn(turn);
+
+      timestamp = millis();
     }
 
+    if (millis() - timestamp > TIMEOUT) {
+      motorSpeed(0);
+      timestamp = millis();
+    }
+    
     //Serial.println(speeds+turn);
     //Serial.println(speeds-turn);
     //Serial.println(speeds);
     //Serial.println(turn);
 }
+
+void motorSpeed(int new_speed) {
+  motor_speed = new_speed;
+  updateMotors();
+}
+
+void motorTurn(int new_turn) {
+  motor_turn = new_turn;
+  updateMotors();
+}
+
+void updateMotors() {
+  if (motor_speed >= 0) {
+    digitalWrite(RIGHT_FORWARD, HIGH);
+    digitalWrite(RIGHT_BACKWARD, LOW);
+    digitalWrite(LEFT_FORWARD, HIGH);
+    digitalWrite(LEFT_BACKWARD, LOW);
+  } else {
+    digitalWrite(RIGHT_FORWARD, LOW);
+    digitalWrite(RIGHT_BACKWARD, HIGH);
+    digitalWrite(LEFT_FORWARD, LOW);
+    digitalWrite(LEFT_BACKWARD, HIGH);
+  }
+  analogWrite(RIGHT_ENABLE, 25 * abs(motor_speed - motor_turn));
+  analogWrite(LEFT_ENABLE, 25 * abs(motor_speed + motor_turn));
+}
+
